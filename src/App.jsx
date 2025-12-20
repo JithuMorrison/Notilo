cabec7a743b076f9fa1a38f73f151e921ed3293c
JithuMorrison <jithus2004@gmail.com>
2025-12-20 13:37:11 +0530

    feat: modified app

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 import FolderStructure from './folders';
 import FileEditor from './file';
 import './App.css';
+import WordEditor from './make';
 
 function App() {
   const [folders, setFolders] = useState([]);
   const [selectedFile, setSelectedFile] = useState(null);
   const [currentPath, setCurrentPath] = useState([]);
+  const [currentSection, setCurrentSection] = useState('notes');
+
+  const toggleSection = () => {
+    setCurrentSection(prevSection => prevSection === 'notes' ? 'make' : 'notes');
+  };
 
   // Load data from localStorage on initial render
   useEffect(() => {
@@ -38,25 +44,31 @@ function App() {
 
   return (
     <div className="app">
-      <div className="sidebar">
-        <FolderStructure 
-          folders={folders} 
-          setFolders={setFolders}
-          selectedFile={selectedFile}
-          setSelectedFile={setSelectedFile}
-          currentPath={currentPath}
-          setCurrentPath={setCurrentPath}
-        />
-      </div>
-      <div className="main-content">
-        <FileEditor 
-          selectedFile={selectedFile} 
-          setSelectedFile={setSelectedFile}
-          folders={folders}
-          setFolders={setFolders}
-          currentPath={currentPath}
-        />
-      </div>
+      <button style={{position:'absolute', top:'10px',left:'320px',zIndex:'99'}} className='gbtn' onClick={toggleSection}>Make Page</button>
+      {currentSection === 'make' && (<WordEditor />)}
+      {currentSection === 'notes' && (
+        <>
+          <div className="sidebar">
+            <FolderStructure 
+              folders={folders} 
+              setFolders={setFolders}
+              selectedFile={selectedFile}
+              setSelectedFile={setSelectedFile}
+              currentPath={currentPath}
+              setCurrentPath={setCurrentPath}
+            />
+          </div>
+          <div className="main-content">
+            <FileEditor 
+              selectedFile={selectedFile} 
+              setSelectedFile={setSelectedFile}
+              folders={folders}
+              setFolders={setFolders}
+              currentPath={currentPath}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
